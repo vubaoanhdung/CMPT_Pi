b7bc22b85fe584e68643b5521b3f1a8cb99101d1
Damon <vubaoanhdung@gmail.com>
2022-02-15 18:58:48 -0600

    first commit - finished basic work flow and the bash script

---
+++ change_temperature.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]
+then
+    echo "usage: ./change_temperature min_temperature max_temperature"
+else
+    
+    file=dht_test.py
+    min_value=$1
+    max_value=$2
+
+    #  check min_value
+    if [ $min_value -gt $max_value ]
+    then
+        echo "Error: min_temperature cannot be greater than max_temperature"
+        exit
+    fi
+
+    # check max_value
+    if [ $max_value -lt $min_value ]
+    then
+        echo "Error: max_temperature cannot be less than min_temperature"
+        exit
+    fi
+
+    # check temperature range
+    if [ $min_value -lt 19 ] || [ $max_value -lt 19 ] || [ $min_value -gt 26 ] || [ $max_value -gt 26 ]
+    then
+        echo "Error: temperature must be in range [19, 26]"
+        exit
+    fi
+
+    # if passing all checks, continue the execution
+    sed -i "s/min_value=.*/min_value='$min_value'/" $file
+    sed -i "s/max_value=.*/max_value='$max_value'/" $file
+
+    # kill the current running program
+    pkill -9 -f dht_test.py
+
+    # restart the program
+    python3 dht_test.py
+fi
---
+++ dht_test.py
@@ -0,0 +1,69 @@
+"""
+    Damon Vu
+    11261393
+    bav965
+    ----------
+    Raspberry Pi + DHT22 Temperature and Humidifier Sensor
+"""
+
+
+import time
+import board
+import adafruit_dht
+
+# Initial the dht device, with data pin connected to:
+# dhtDevice = adafruit_dht.DHT22(board.D18)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+# Set min_temperature and max_temperature if user called change_temperature.sh
+min_value='19'
+max_value='25'
+
+# Preferred Temperature
+min_temp = int(min_value) 
+max_temp = int(max_value);
+frequency_reading = 10
+
+# print the current setting
+print("\t\tCURRENT SETTING")
+print("\t\tMIN TEMP = " + str(min_temp))
+print("\t\tMAX TEMP = " + str(max_temp))
+print("\t\tFREQUENCY READING: 10s")
+
+# New line
+print()
+
+print("START COLLECTING DATA...")
+
+# New line
+print()
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        # print(error.args[0])
+        time.sleep(2.0)
+        continue
+
+    # If the user kills the program, exit
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    # Read data from sensor every frequency_reading seconds (if no error occurs)
+    time.sleep(frequency_reading)
