a9920ab1baa204e10797f9c00748031109027162
Damon <vubaoanhdung@gmail.com>
2022-02-23 18:40:16 -0600

    added test_lambda_invoke.py

---
+++ test_lambda_invoke.py
@@ -0,0 +1,40 @@
+import io
+import json
+import logging
+import random
+import time
+import zipfile
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+def invoke_lambda_function(lambda_client, function_name, function_params):
+    """
+    Invokes an AWS Lambda function.
+    :param lambda_client: The Boto3 AWS Lambda client object.
+    :param function_name: The name of the function to invoke.
+    :param function_params: The parameters of the function as a dict. This dict
+                            is serialized to JSON before it is sent to AWS Lambda.
+    :return: The response from the function invocation.
+    """
+    try:
+        response = lambda_client.invoke(
+            FunctionName=function_name,
+            Payload=json.dumps(function_params).encode())
+        logger.info("Invoked function %s.", function_name)
+    except ClientError:
+        logger.exception("Couldn't invoke function %s.", function_name)
+        raise
+    return response
+
+lambda_client = boto3.client('lambda')
+lambda_params = {
+    "mode": "HEAT",
+    "temperature": 19
+}
+lambda_function_name = 'changeThermostatSetting'
+
+response = invoke_lambda_function(lambda_client, lambda_function_name, lambda_params)
+print(json.load(response['Payload']))
+        
